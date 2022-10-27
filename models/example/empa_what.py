7882b6eda05417efb42c2bb76dc2cf28af11457b
Ernesto Ongaro <ernesto.ongaro@fishtownanalytics.com>
2022-10-27 12:12:51 +0000

    hack multiple models into one py file

--- models/example/empa_what.py
+++ models/example/empa_what.py
@@ -4,8 +4,10 @@ def model(dbt, session):
         materialized = "table", 
     )
 
-
-    df = session.create_dataframe([1, 2, 3, 4]).to_df("empanadas")
+    df = session.create_dataframe([1, 2, 3, 4]).to_df("something")
+    df.write.mode("overwrite").save_as_table(dbt.this.database + '.' +  dbt.this.schema+ '.' + "hack_this_1", create_temp_table=False)
+    df.write.mode("overwrite").save_as_table(dbt.this.database + '.' +  dbt.this.schema+ '.' + "hack_this_2", create_temp_table=False)
+    df.write.mode("overwrite").save_as_table(dbt.this.database + '.' +  dbt.this.schema+ '.' + "hack_this_3", create_temp_table=False)
 
 
     return df
