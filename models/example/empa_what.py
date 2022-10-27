f31ef828a61e113753f6d0fc8c24f59c03666452
Ernesto Ongaro <ernesto.ongaro@fishtownanalytics.com>
2022-10-27 09:51:07 +0000

    fixes empanadas

--- models/example/empa_what.py
+++ models/example/empa_what.py
@@ -5,7 +5,7 @@ def model(dbt, session):
     )
 
 
-    df1 = session.create_dataframe([1, 2, 3, 4]).to_df("empanadas")
+    df = session.create_dataframe([1, 2, 3, 4]).to_df("empanadas")
 
 
     return df
