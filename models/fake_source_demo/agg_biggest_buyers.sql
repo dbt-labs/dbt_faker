b6d3ff4130fc3c73986cab735c0fb6421edca60d
Victoria Perez Mola <victoria.mola@fishtownanalytics.com>
2022-10-28 11:35:42 +0000

    update models

--- models/fake_source_demo/agg_biggest_buyers.sql
+++
@@ -1,7 +0,0 @@
-select 
-    customer_id,
-    count(*) as customer_item_count
-from {{ source('erp', 'order_items') }} as order_items
-left join {{ source('erp', 'orders') }} as orders
-    on orders.order_id = order_items.order_id
-group by 1
