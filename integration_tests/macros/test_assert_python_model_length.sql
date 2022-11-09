d85f660a02ab8446925394cbd7bbdc3210671961
Ernesto Ongaro <ernestoongaro@gmail.com>
2022-11-09 15:52:41 +0000

    adds basic integration tests (#24)
    * adds basic integration tests
    
    * starting automation, some changes to use print, so we can use the quiet flag later
    
    * fix workflow
    
    * Create profiles.yml
    
    * Create unit-test.yml

---
+++ integration_tests/macros/test_assert_python_model_length.sql
@@ -0,0 +1,7 @@
+{% macro test_assert_python_model_length() %}
+
+ {# "super basic check that we're generating something" #}
+ {% set python_model = dbt_faker.generate_faker_model() %}
+ {{ dbt_unittest.assert_true(python_model | length > 50) }}
+
+{% endmacro %}
