47aa0001dde8cfa53bfb0f065848bb78916d7699
Ernesto Ongaro <ernestoongaro@gmail.com>
2022-11-10 16:33:30 +0000

    Integration tests (#25)
    * adds basic integration tests
    
    * starting automation, some changes to use print, so we can use the quiet flag later
    
    * fix workflow
    
    * valid python version
    
    * adds more tests, tweaks
    
    * indentation

--- macros/generate_faker_model.sql
+++ macros/generate_faker_model.sql
@@ -4,71 +4,71 @@
 
     {% set fake_model_py %}
 
-    import faker
-    import pandas
-    from snowflake.snowpark import Row
+import faker
+import pandas
+from snowflake.snowpark import Row
 
 
-    def create_rows(dbt, session, source_name, table_name, num=1, **kwargs):
-        fake = faker.Faker()
+def create_rows(dbt, session, source_name, table_name, num=1, **kwargs):
+    fake = faker.Faker()
 
-        df = session.create_dataframe([
-            Row(**{
-                key: getattr(fake, value)()
-                for key, value in kwargs.items()
-            }) for x in range(num)
-        ])
+    df = session.create_dataframe([
+        Row(**{
+            key: getattr(fake, value)()
+            for key, value in kwargs.items()
+        }) for x in range(num)
+    ])
 
-        df.write.mode("overwrite").save_as_table(
-            f"{dbt.this.database}.{dbt.this.schema}.fake__{source_name}__{table_name}",
-            create_temp_table=False,
-        )
+    df.write.mode("overwrite").save_as_table(
+        f"{dbt.this.database}.{dbt.this.schema}.fake__{source_name}__{table_name}",
+        create_temp_table=False,
+    )
 
-    def model(dbt, session):
-        dbt.config(
-        materialized="table", 
-        packages=["faker", "pandas"],
-        )
+def model(dbt, session):
+    dbt.config(
+    materialized="table",
+    packages=["faker", "pandas"],
+    )
 
-    {%- for source_table in final_list -%}
-        {%- set columns = source_table.columns -%}
-        {% set column_names=columns %}
-        {% set unique_id=source_table['unique_id'] %}
 
-        create_rows(
-        dbt,
-        session,
-        {%- if source_table['meta'].faker_rows -%}
-            {%- set def_fake_rows=source_table['meta'].faker_rows -%}
-        {%- else -%}
-            {%- set def_fake_rows=100 -%}
-        {%- endif %}        
-        num={{ def_fake_rows }},
-        source_name='{{ unique_id.split(".")[-2]  }}',
-        table_name='{{ unique_id.split(".")[-1]  }}',
+{%- for source_table in final_list -%}
+    {%- set columns = source_table.columns -%}
+    {% set column_names=columns %}
+    {% set unique_id=source_table['unique_id'] %}
 
-        {%- for column in column_names  %}
-        {%- set def_fake_provider=column_names[column]['meta'].faker_provider %}
-        {%- if (def_fake_provider|length) == 0 %}
-            {%- set def_fake_provider='pystr' %}
-        {%- endif %}
-        {{ column_names[column]['name'] | upper }}{{ "='" ~ def_fake_provider ~ "'" }}{{"," if not loop.last}}
-        {%- endfor %}
-        )
+    create_rows(
+    dbt,
+    session,
+    {%- if source_table['meta'].faker_rows -%}
+        {%- set def_fake_rows=source_table['meta'].faker_rows -%}
+    {%- else -%}
+        {%- set def_fake_rows=100 -%}
+    {%- endif %}        
+    num={{ def_fake_rows }},
+    source_name='{{ unique_id.split(".")[-2]  }}',
+    table_name='{{ unique_id.split(".")[-1]  }}',
 
-    {% endfor %}
-    
-        df = session.create_dataframe(['yes']).to_df("are_we_faking")
+    {%- for column in column_names  %}
+    {%- set def_fake_provider=column_names[column]['meta'].faker_provider %}
+    {%- if (def_fake_provider|length) == 0 %}
+        {%- set def_fake_provider='pystr' %}
+    {%- endif %}
+    {{ column_names[column]['name'] | upper }}{{ "='" ~ def_fake_provider ~ "'" }}{{"," if not loop.last}}
+    {%- endfor %}
+    )
 
-        return df
+{% endfor %}
 
-    {% endset %}
+    df = session.create_dataframe(['yes']).to_df("are_we_faking")
+    return df
 
-    {% if execute %}
+{% endset %}
 
-    {{ print(fake_model_py) }}
-    {% do return(fake_model_py) %}
+{% if execute %}
 
-    {% endif %}
+{{ print(fake_model_py) }}
+{% do return(fake_model_py) %}
+
+{% endif %}
 
 {% endmacro %}
